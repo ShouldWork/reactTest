7567451a49efa815ab3ea8b09e74c15e4a0ef1a8
Blake <mrblocke@gmail.com>
2019-01-10 10:29:53 -0700

    Updates

---
+++ precache-manifest.2a57105756f3e9aa23f9ef90403f0d8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9b341953a1816f14b995",
+    "url": "/static/js/main.9b341953.chunk.js"
+  },
+  {
+    "revision": "c241c8e9abb2bfbc30c2",
+    "url": "/static/js/1.c241c8e9.chunk.js"
+  },
+  {
+    "revision": "9b341953a1816f14b995",
+    "url": "/static/css/main.e63f2cf8.chunk.css"
+  },
+  {
+    "revision": "d372a3147c95455921a0a39dc52ecef1",
+    "url": "/index.html"
+  }
+];
